2316c7de060a8a5b5263f26992f5e0472f5e5725
dawnik <qsmiespa@wp.pl>
2019-12-17 20:34:02 +0100

    added databases=False on django_heroku in settings

--- core_app/settings.py
+++ core_app/settings.py
@@ -129,4 +129,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'training_polls:login_page'
 LOGIN_URL = 'login'
-django_heroku.settings(locals())
+django_heroku.settings(locals(), databases=False)
